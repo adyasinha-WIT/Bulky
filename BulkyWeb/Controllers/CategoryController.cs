33d8374f6e8ae9e6f91e9680d5ef893fc0ba55ab
AdyaS <118127954+adyasinha-WIT@users.noreply.github.com>
2025-03-14 13:48:11 +1300

    updating design

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,6 +16,17 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
     }
 }
