8afaa07a1ae203cfede45c96a781289b68ee5df7
AdyaS <118127954+adyasinha-WIT@users.noreply.github.com>
2025-05-15 23:26:20 +1200

    Adding company controller, irepository and Repository

--- SD7501Bulky.DataAccess/Repository/IRepository/ICompanyRepository.cs
+++ SD7501Bulky.DataAccess/Repository/IRepository/ICompanyRepository.cs
@@ -1,18 +1,17 @@
-﻿using System;
+﻿using Bulky.Models;
+using BulkyWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork
+    public interface ICompanyRepository : IRepository<Company>
     {
-
-        ICategoryRepository Category { get; }
-        IProductRepository Product { get; }
-        ICompanyRepository Company { get; }
+        void Update(Company obj);
         void Save();
+
     }
 }
