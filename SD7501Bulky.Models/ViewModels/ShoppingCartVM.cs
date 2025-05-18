c8fff492781d2f364287b5f4281b5f6d3ceb2099
AdyaS <118127954+adyasinha-WIT@users.noreply.github.com>
2025-05-18 21:44:22 +1200

    changes

--- SD7501Bulky.Models/ViewModels/ShoppingCartVM.cs
+++ SD7501Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 
 namespace Bulky.Models.ViewModels
 {
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
     }
 }
